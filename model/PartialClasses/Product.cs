9741da43bd2c8d7731de93f1706f40dcbdd2759b
rotoyouoi <rotoyouoi@yandex.ru>
2023-02-06 15:05:46 +0500

    Добавьте файлы проекта.

---
+++ model/PartialClasses/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tires.model
+{
+    public partial class Product
+    {
+        public string ImagePath
+        {
+            get
+            {
+                if (Image == null)
+                {
+                    return "..\\..\\Resources\\picture.png";
+                }
+                else
+                {
+
+                    return "..\\..\\Resources\\" + Image;
+                }
+
+            }
+        }
+        public string MaterialList
+        {
+            get
+            {
+                string materials = "";
+                return materials;
+            }
+        }
+    }
+}
